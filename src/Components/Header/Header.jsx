aa1b040762e597fc298f5b69d613e42eac2c69ea
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-09 01:12:18 +0600

    showing date fromo date fns

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className='flex justify-center flex-col gap-3 items-center'>
-      <img src={logo} alt="" />
+      <img className='mt-7' src={logo} alt="" />
       <p className='text-accent'> Journalism Without Fear or Favour</p>
+      <p className='font-semibold text-accent'>{format(new Date (),"EEEE, MMMM MM,  YYY" )}</p>
+      
     </div>
   );
 };
