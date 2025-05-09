7763eb8151cf73e34e8b5e8051823b94e94d3885
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-09 17:49:30 +0600

    news filtered added

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
-  const {id} = useParams();
+  const [categoryNews, setCategoryNews] = useState([]);
+  const { id } = useParams();
 
-  const  data = useLoaderData();
+  const data = useLoaderData();
 
-  console.log(data)
+
+  // console.log(data)
+
+  useEffect(() => {
+
+    if (id == 0) {
+      return setCategoryNews(data)
+    }
+    else if (id == 1) {
+      const filteredNews = data.filter(news => news.others.is_today_pick == true)
+      return setCategoryNews(filteredNews)
+    }
+
+    else {
+      // ei khane equal duita  dichi karon duitar type alada 
+      const filteredNews = data.filter(news => news.category_id == id)
+
+      // console.log(filteredNews)
+      setCategoryNews(filteredNews)
+    }
+    // karon data and id duitai antechi baire theke 
+  }, [data, id])
+
   return (
-  
+
     <div>
-      CategoryNews--{id}
+      <p>Total {categoryNews.length} news found</p>
     </div>
   );
 };
 
 export default CategoryNews;
+    
+
+
+
+
