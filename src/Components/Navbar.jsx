c307a4403cd2b45aa423cabf6bf1c53e1b946f5b
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-10 13:41:47 +0600

    demo context set up

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link, NavLink } from 'react-router';
 import user from "../assets/user.png"
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const Navbar = () => {
+  const {user} = use(AuthContext)
   return (
     <div className='flex  justify-between items-center mt-3'>
-     <div className="blank-div"></div>
+     <div className="blank-div">{user.email}</div>
      <div className="nav-iteam flex gap-3 text-accent">
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/about'>About</NavLink>
